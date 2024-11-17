40c716f30356aa2c55537063cd52cf9c0316a47c
naya <rosyahw@gmail.com>
2024-11-17 19:03:01 +0700

    first commit

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,108 @@
+export const getAllProducts = () => {
+  return [
+    {
+      id: 'SHHN-001',
+      slug: 'NB1',
+      name: "New Balance 530 (White/Indigo)",
+      category: 'Unisex Shoe',
+      price: 3_150_000,
+      stock: 25,
+      imageUrl: '/assets/images/NB1.jpg',
+      description: `NEW BALANCE 530 SILVER WHITE METALLIC
+      BRAND NEW IN BOX
+      Article Code : MR530A
+      Released : 2023
+      Tag : Made in Vietnam
+      Colorway : White/Metallic Silver`,
+      size: "37-41",
+      
+    },
+    {
+      id: 'SHHN-002',
+      slug: 'NB2',
+      name: "New Balance 550 (Blue Sneakers)",
+      category: 'Best Seller',
+      price: 1_200_000,
+      stock: 7,
+      imageUrl: '/assets/images/NB2.jpg',
+      description: `The classic styling of the New Balance 550 UNC White University Blue offers a retro look with traditional basketball oxford styling elements. They feature the classic "N" styling on the sides of the sneaker plus traditional New Balance branding on the tongue of the shoes. These predominantly white shoes have subtle yet distinct touches of North Carolina baby blue on the soles, tongue, and logo to give them an understated yet vibrant look.`,
+      size: "39-44",
+      
+    },
+    {
+      id: 'SHHN-003',
+      slug: 'NB3',
+      name: "New Balance 550 (White Green)",
+      category: 'Popular',
+      price: 1_120_000,
+      stock: 13,
+      imageUrl: '/assets/images/NB3.jpg',
+      description: `The original 550 debuted in 1989 and made its mark on basketball courts from side to side. After its initial launch, the 550 was tucked away in the archives, before being reintroduced in a limited edition release in late 2020, and returning to the full-time lineup in 2021, quickly becoming a global fashion favourite. The familiar 550 silhouette is complemented by a premium leather upper, presented in a monochromatic colorway, for an attractive classic look.`,
+      size: "40-42",
+      
+    },
+    {
+      id: 'SHHN-004',
+      slug: 'NB4',
+      name: "New Balance 550 990v6",
+      category: 'Popular',
+      price: 4_150_000,
+      stock: 30,
+      imageUrl: '/assets/images/NB4.jpg',
+      description: `The designers of the first 990 were tasked with creating the best running shoe on the market. The MADE in USA 990v6 takes on authentic credentials, with a number of updates inspired by its performance. The upper circulates air thanks to a midfoot saddle, allowing the suede and synthetic overlays to channel air from heel to toe through the mesh underlay, for a fast, sleek look. The evolved design is one of the most dramatic changes from one model generation to the next ever seen on the 990, the biggest change being on the inside. The addition of midsole cushioning means the evolution of the 990 can be felt as well as seen.`,
+      size: "38-44",
+      
+    },
+    {
+      id: 'SHHN-005',
+      slug: 'NB5',
+      name: "New Balance 993",
+      category: 'Best Seller',
+      price: 3_400_000,
+      stock: 10,
+      imageUrl: '/assets/images/NB5.jpg',
+      description: `Superior comfort meets classic style in our Made in US 993 men's running shoe. A favorite from the legendary 990 series, the 993 has responsive cushioning and premium materials that are ready for running or all-day wear. Made in the US for over 75 years and representing a limited portion of our US sales, New Balance Made is a premium collection that has a domestic value of 70% or greater.`,
+      size: "40-41",
+      
+    },
+    {
+      id: 'SHHN-006',
+      slug: 'NB6',
+      name: "New Balance 2002R (Protection Pack)",
+      category: 'Popular',
+      price: 2_108_000,
+      stock: 15,
+      imageUrl: '/assets/images/NB6.jpg',
+      description: `Very popular collection, the Protection Pack is back on the model that made it known, the .
+
+      This New Balance 2002R Protection Pack Navy presents a base in navy blue, accompanied by overlays in light blue Sweden. Emporarys in Hairy Sweden Navy Blue effect shredded bring the look deconstruct which appeals to this range so much.`,
+      size: "41",
+      
+    },
+    {
+      id: 'SHHN-007',
+      slug: 'NB7',
+      name: "New Balance Kith X 200R (Pistachio slate grey/chinois)",
+      category: 'Best Seller',
+      price: 1_880_000,
+      stock: 23,
+      imageUrl: '/assets/images/NB7.jpg',
+      description: `2002R silhouette Mesh and suede upper Padded collar Metal hook eyelets Perforated toe box and heel warp NB logo on tongue Embroidered N logo on side panel Debossed 2002R logo on ankle Flat cotton laces ABZORB SBS heel cushioning Rubber outsole`,
+      size: "39-40",
+      
+    },
+    {
+      id: 'SHHN-008',
+      slug: 'NB8',
+      name: "New Balance GS 2002R (Pink Granite/Mid Century Pink)",
+      category: 'Popular',
+      price: 3_250_000,
+      stock: 18,
+      imageUrl: '/assets/images/NB8.jpg',
+      description: `Grade School 2002R silhouette Mesh upper Leather overlay Padded collar TPU eyelets on collar Embroidered N logo on front New Balance branding on tongue N logo patch on side panels 2002R branding on ankle NB logo on rear Perforated heel counter Flat cotton laces N-ergy branding on midsole ABZORB midsole N-ergy rubber outsole`,
+      size: "38-44",
+     
+    },
+
+  ]
+}
